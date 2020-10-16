59f112b88a1f95a894eb1f916d7df330a5f789cc
tcl <1091203156@qq.com>
2020-10-16 09:14:09 +0800

    1

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -3,6 +3,7 @@ package com.atguigu.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//SpringBoot主程序
 @SpringBootApplication
 public class PaymentMain8001 {
     public static void main(String[] args) {
