7020f7dacf74e582f69badac009b58778a8a25db
tcl <1091203156@qq.com>
2020-10-22 17:52:21 +0800

    consul

--- cloud-consumerconsul-order80/src/main/java/com/atguigu/springcloud/controller/OrderConsulController.java
+++ cloud-consumerconsul-order80/src/main/java/com/atguigu/springcloud/controller/OrderConsulController.java
@@ -1,34 +1,24 @@
 package com.atguigu.springcloud.controller;
 
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
-import java.util.Scanner;
 
 @RestController
 @Slf4j
-public class OrderZKController {
-    public static final String INVOKE_URL = "http://cloud-provider-payment";
+public class OrderConsulController {
+    public static final String INVOKE_URL = "http://consul-provider-payment";
 
     @Resource
     private RestTemplate restTemplate;
 
-    @GetMapping(value = "/consumer/payment/zk")
-    public String paymentInfo() {
-        String result = restTemplate.getForObject(INVOKE_URL + "/payment/zk", String.class);
+    @GetMapping(value = "/consumer/payment/consul")
+    public String paymentInfo(){
+        String result = restTemplate.getForObject(INVOKE_URL+"/payment/consul",String.class);
         return result;
     }
-
 }
-
-
-
-
-
-
-
-
-
