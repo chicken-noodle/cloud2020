6d3d48dd13c6cc0b087ce486dfa960249ea9a2eb
shixi.gongxiang15 <shixi.gongxiang15@tcl.com>
2020-12-25 13:36:34 +0800

    OpenFeign

--- cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/controller/OrderFeignController.java
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/controller/OrderFeignController.java
@@ -6,6 +6,7 @@ import com.atguigu.springcloud.service.PaymentFeignService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -19,6 +20,11 @@ public class OrderFeignController {
     @GetMapping(value = "/consumer/payment/get/{id}")
     public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
         return paymentFeignService.getPaymentById(id);
+    }
+
+    @RequestMapping("/consumer/payment/feign/timeout")
+    public String paymentFeignTimeout(){
+        return paymentFeignService.paymentFeignTimeout();
     }
 
 }
