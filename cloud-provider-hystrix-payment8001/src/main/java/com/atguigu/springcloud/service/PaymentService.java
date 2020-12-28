b15c5d6d87f8668026cfc4c0756d8e9ae6c0b24c
shixi.gongxiang15 <shixi.gongxiang15@tcl.com>
2020-12-28 13:28:38 +0800

    Hystrix 8001服务降级

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.atguigu.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -9,6 +11,10 @@ public class PaymentService {
     public String paymentInfo_OK(Integer id){
         return "线程池："+Thread.currentThread().getName()+" paymentInfo_OK,id: "+id;
     }
+
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "3000")
+    })
     public String paymentInfo_TimeOut(Integer id){
         try {
             TimeUnit.SECONDS.sleep(3);
@@ -16,6 +22,10 @@ public class PaymentService {
             e.printStackTrace();
         }
         return "线程池："+Thread.currentThread().getName()+" paymentInfo_OK,id: "+id+" 三秒";
+    }
+
+    public String paymentInfo_TimeOutHandler(Integer id){
+        return "线程池："+Thread.currentThread().getName()+" paymentInfo_TimeOutHandler,id: "+id+"o(╥﹏╥)o";
     }
 
 }
